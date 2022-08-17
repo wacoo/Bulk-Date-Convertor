49dfbae4dbedaa60c36d3fd35c290691635821ed
wac <wabaham9@gmail.com>
2022-08-17 05:23:01 -0700

    initial commit

---
+++ GC EC Bulk Date Convertor/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lstStart = New System.Windows.Forms.ListBox()
+        Me.lstDest = New System.Windows.Forms.ListBox()
+        Me.cmbChange = New System.Windows.Forms.ComboBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.txtFilePath = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtText = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnChange = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstStart
+        '
+        Me.lstStart.FormattingEnabled = True
+        Me.lstStart.Location = New System.Drawing.Point(127, 222)
+        Me.lstStart.Name = "lstStart"
+        Me.lstStart.Size = New System.Drawing.Size(209, 186)
+        Me.lstStart.TabIndex = 2
+        '
+        'lstDest
+        '
+        Me.lstDest.FormattingEnabled = True
+        Me.lstDest.Location = New System.Drawing.Point(447, 221)
+        Me.lstDest.Name = "lstDest"
+        Me.lstDest.Size = New System.Drawing.Size(209, 186)
+        Me.lstDest.TabIndex = 3
+        '
+        'cmbChange
+        '
+        Me.cmbChange.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbChange.FormattingEnabled = True
+        Me.cmbChange.Items.AddRange(New Object() {"Gregorian to Ethiopic", "Ethiopic to Gregorian"})
+        Me.cmbChange.Location = New System.Drawing.Point(127, 184)
+        Me.cmbChange.Name = "cmbChange"
+        Me.cmbChange.Size = New System.Drawing.Size(209, 21)
+        Me.cmbChange.TabIndex = 4
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(127, 419)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(529, 34)
+        Me.btnSave.TabIndex = 5
+        Me.btnSave.Text = "Save to File"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'txtFilePath
+        '
+        Me.txtFilePath.Location = New System.Drawing.Point(124, 44)
+        Me.txtFilePath.Name = "txtFilePath"
+        Me.txtFilePath.Size = New System.Drawing.Size(462, 20)
+        Me.txtFilePath.TabIndex = 7
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(593, 43)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(60, 23)
+        Me.btnBrowse.TabIndex = 8
+        Me.btnBrowse.Text = "Browse..."
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(124, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(125, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Select File (MM/dd/yyyy)"
+        '
+        'txtText
+        '
+        Me.txtText.Location = New System.Drawing.Point(124, 93)
+        Me.txtText.Name = "txtText"
+        Me.txtText.Size = New System.Drawing.Size(462, 20)
+        Me.txtText.TabIndex = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(121, 77)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(126, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Type Date (MM/dd/yyyy)"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(124, 166)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Converion Type"
+        '
+        'btnChange
+        '
+        Me.btnChange.Location = New System.Drawing.Point(354, 289)
+        Me.btnChange.Name = "btnChange"
+        Me.btnChange.Size = New System.Drawing.Size(75, 51)
+        Me.btnChange.TabIndex = 13
+        Me.btnChange.Text = "Convert >"
+        Me.btnChange.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Enabled = False
+        Me.btnAdd.Location = New System.Drawing.Point(508, 119)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(78, 24)
+        Me.btnAdd.TabIndex = 14
+        Me.btnAdd.Text = "Add Date"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(761, 464)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnChange)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtText)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtFilePath)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.cmbChange)
+        Me.Controls.Add(Me.lstDest)
+        Me.Controls.Add(Me.lstStart)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "GC EC Convertor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstStart As ListBox
+    Friend WithEvents lstDest As ListBox
+    Friend WithEvents cmbChange As ComboBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents txtFilePath As TextBox
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtText As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnChange As Button
+    Friend WithEvents btnAdd As Button
+End Class
---
+++ GC EC Bulk Date Convertor/Form1.vb
@@ -0,0 +1,103 @@
+﻿Imports System.IO
+Imports System.Windows.Forms
+
+Public Class Form1
+    Dim path As String = ""
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Try
+            Dim sfd As New SaveFileDialog
+            sfd.Filter = "Text files (*.txt)|*.txt|CSV files (*.csv)|*.csv|All files (*.*)|*.*"
+            sfd.FilterIndex = 1
+            sfd.RestoreDirectory = True
+            'Prompt the user for a file name to save to.
+            If sfd.ShowDialog() = DialogResult.OK Then
+                'Create the file to save to.
+                Dim output As New IO.StreamWriter(sfd.FileName)
+
+                'Write lines of text to the file.
+                For Each item As Object In lstDest.Items
+                    output.WriteLine(lstDest.GetItemText(item))
+                Next item
+                MsgBox("Saved Successfully!", MsgBoxStyle.Information)
+                'Close the file.
+                output.Close()
+            End If
+        Catch x As Exception
+            MsgBox("There is something wrong with the saving!" + x.StackTrace, MsgBoxStyle.Exclamation)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        Try
+            lstStart.Items.Clear()
+            txtFilePath.Enabled = True
+            Dim od As OpenFileDialog = New OpenFileDialog()
+            od.Filter = "Text File (*.txt;*.csv)|*.txt;*.csv"
+            od.FilterIndex = 0
+            Dim result As DialogResult = od.ShowDialog()
+            ' Test result.
+            If result = System.Windows.Forms.DialogResult.OK Then
+
+                ' Get the file name.
+                path = od.FileName
+                txtFilePath.Text = path
+                Dim txt As String
+                Dim objReader As New System.IO.StreamReader(path)
+                Do While objReader.Peek() <> -1
+                    txt = objReader.ReadLine() & vbNewLine
+                    lstStart.Items.Add(txt)
+                Loop
+
+            End If
+        Catch x As Exception
+            MsgBox("There is something wrong with reading the file!" + x.StackTrace, MsgBoxStyle.Exclamation)
+        End Try
+    End Sub
+
+    Private Sub txtText_MouseEnter(sender As Object, e As EventArgs) Handles txtText.MouseEnter
+        txtText.Enabled = True
+        If Not String.IsNullOrEmpty(txtFilePath.Text) Then
+            txtFilePath.Enabled = False
+        End If
+    End Sub
+
+    Private Sub txtFilePath_TextChanged(sender As Object, e As EventArgs) Handles txtFilePath.TextChanged
+        If Not String.IsNullOrEmpty(txtText.Text) Then
+            txtText.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnChange_Click(sender As Object, e As EventArgs) Handles btnChange.Click
+        Try
+            lstDest.Items.Clear()
+            Dim hdp As HabeshaDateTimePicker.EthiopianDateControl = New HabeshaDateTimePicker.EthiopianDateControl
+            'Dim dt As Date = #8/17/2022#
+            Dim dt As Date
+            'Console.WriteLine("DDDD" + hdp.ConvertGregorianToEthiopianDate(d, True))
+            If cmbChange.SelectedIndex.Equals(0) Then
+                For Each i As String In lstStart.Items
+                    lstDest.Items.Add(hdp.ConvertGregorianToEthiopianDate(i, True))
+                Next
+            ElseIf cmbChange.SelectedIndex.Equals(1) Then
+                For Each i As String In lstStart.Items
+                    lstDest.Items.Add(hdp.ConvertEthiopianDateToGregorianDate(i, True))
+                Next
+            End If
+        Catch x As Exception
+            MsgBox("Error: Check date is the correct format MM/dd/yyyy!" + x.StackTrace, MsgBoxStyle.Exclamation)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbChange.SelectedIndex = 0
+    End Sub
+
+    Private Sub txtText_TextChanged(sender As Object, e As EventArgs) Handles txtText.TextChanged
+        btnAdd.Enabled = True
+        txtFilePath.Enabled = False
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
